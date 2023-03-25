782f2b9edc0180d887a1e611319a31a30c7676ed
AHX <AHX@DESKTOP-SPN8ONV>
2023-03-25 12:17:03 +0300

    Добавьте файлы проекта.

---
+++ ASP.NET Training/Controllers/CategoryController.cs
@@ -0,0 +1,90 @@
+﻿using ASP.NET_Training.Data;
+using ASP.NET_Training.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASP.NET_Training.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Category> objCategoryList = _db.Categories;
+            return View(objCategoryList);
+        }
+
+        public IActionResult Create() 
+        {
+            return View();
+        }
+
+        //Post
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category obj)
+        {
+            if(obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The DisplayOrder cannot exactly match the Name");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                TempData["success"] = "Category Created Succesfully";
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0) { return NotFound(); }
+            var categoryFromDb = _db.Categories.Find(id);
+            if(categoryFromDb == null) { return NotFound(); }
+            return View(categoryFromDb);
+        }
+        //Post
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The DisplayOrder cannot exactly match the Name");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                TempData["success"] = "Category Edited Succesfully";
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0) { return NotFound(); }
+            var categoryFromDb = _db.Categories.Find(id);
+            if (categoryFromDb == null) { return NotFound(); }
+            return View(categoryFromDb);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePOST(int? id)
+        {
+            var categoryFromDb = _db.Categories.Find(id);
+            if (categoryFromDb == null) { return NotFound(); }
+            _db.Categories.Remove(categoryFromDb);
+            _db.SaveChanges();
+            TempData["success"] = "Category Deleted Succesfully";
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ ASP.NET Training/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using ASP.NET_Training.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ASP.NET_Training.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base (options)
+        {
+            
+        }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
